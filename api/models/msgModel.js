cbaa322ebe9800e852d785b32a08b2cecc0ad34d
cxzhang <chenxizhang@lionfin.com.hk>
2017-11-29 16:22:41 +0800

    save msg

--- api/models/msgModel.js
+++ api/models/msgModel.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const MsgSchema = new Schema({
+  chat: Schema.Types.Mixed,
+  tags: [String],
+  raw: Schema.Types.Mixed,
+  type: String,
   created_date: {
     type: Date,
     default: Date.now
-  },
-  msg: Schema.Types.Mixed
+  }
 })
 
 module.exports = mongoose.model('Msg', MsgSchema)
--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,12 @@
 const Telegraf = require('telegraf')
 
+const mongoose = require('mongoose'),
+  Msg = require('../api/models/msgModel'),
+  dashboard = require('../api/controllers/dashController')
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/TelegramChannelDB')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
@@ -11,6 +18,7 @@ bot.start((ctx) => {
 bot.on(['message', 'channel_post'], (ctx) => {
   if (ctx.channelPost) {
     console.log(ctx.channelPost)
+    dashboard.create_a_msg(ctx.channelPost)
     // ctx.reply(ctx.channelPost)
   }
 })
