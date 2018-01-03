df1dc180611450532558fed013c420150564f08b
cxzhang <chenxizhang@lionfin.com.hk>
2018-01-03 14:52:51 +0800

    tag

--- client/src/components/CardModal/Detail/index.jsx
+++ client/src/components/CardModal/Detail/index.jsx
@@ -4,19 +4,24 @@ import _isEmpty from 'lodash/isEmpty'
 import _get from 'lodash/get'
 
 import { getCleanText, formatDate } from '~/utils'
+import Tag from '~/components/Tag'
 import styles from './Detail.css'
 
 const Detail = ({ msg }) => {
   const imgSrc = _get(msg, 'preview.mercury.lead_image_url')
   const markContent = _get(msg, 'preview.mark.text')
+  const onTagClick = () => ({})
 
   return (
     _isEmpty(msg)
       ? null
       : <div className={styles.container}>
         <div className={styles.left}>
-          { imgSrc && <img className={styles.leadImg} src={imgSrc} alt='lead-img' /> }
-          <div className={styles.main} dangerouslySetInnerHTML={{__html: getCleanText(msg.raw.text)}} />
+          <div className={styles.msg}>
+            { imgSrc && <img className={styles.leadImg} src={imgSrc} alt='lead-img' /> }
+            <div className={styles.main} dangerouslySetInnerHTML={{__html: getCleanText(msg.raw.text)}} />
+          </div>
+          <ul>{msg.tags.map(tag => <Tag text={tag} onTagClick={onTagClick} />)}</ul>
         </div>
         {
           markContent &&
